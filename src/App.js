69392b5cc82e78dd3f7bb8b36e285092e0ada4be
Thiago Lino <thiagolinocsm@hotmail.com>
2021-06-22 01:13:17 -0300

    Buscando dados externos com fech

--- src/App.js
+++ src/App.js
@@ -3,61 +3,48 @@ import './App.css';
 
 class App extends Component {
   state = {
-    counter: 0,
-    posts: [
-      {
-        id: 1,
-        title: 'O título 1',
-        body: 'O corpo 1'
-      },
-      {
-        id: 2,
-        title: 'O título 2',
-        body: 'O corpo 2'
-      },
-      {
-        id: 3,
-        title: 'O título 3',
-        body: 'O corpo 3'
-      },
-    ]
+    posts: []
   };
-  timeoutUpdate = null; //Declarando timeoutUpdate par poder acessa-lo
 
-  componentDidMount() { //Quando o componente acaba de ser montado esse metodo é executado
-    this.handleTimeout();
+  componentDidMount() {
+    this.loadPosts();
   }
 
-  componentDidUpdate() { //Quando o componente é atualizado esse metodo é executado
-    this.handleTimeout();
-  }
+  loadPosts = async () => {
+    //fazendo requisição da api externa e atribuindo a constante
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
 
-  componentWillUnmount() { //Quando o componente é desmontado esse metodo é executado
-    clearTimeout(this.timeoutUpdate);//limpar lixo que pode ser gerado na pagina
-  }
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]); //promessa de resposta
 
-  handleTimeout = () => { //metodo criado para mudar o titulo do primeiro post e o contador
-    const { posts, counter } = this.state;
-    posts[0].title = 'O título mudou';
+    //convertendo para json
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
 
-    this.timeoutUpdate = setTimeout(() => {
-      this.setState({ posts, counter: counter + 1 });
-    }, 1000);
+    const postsAndPhotos = postsJson.map((post, index) => { //unir dois arrays colocando o menor primeiro
+      return { ...post, cover: photosJson[index].url } //unir o objeto com a foto
+    });
+
+    this.setState({ posts: postsAndPhotos }); //set para estado
   }
 
   render() {
-    const { posts, counter } = this.state;
+    const { posts } = this.state;
 
     return (
-      <div className="App">
-        <h1>{counter}</h1>
-        {posts.map(post => (
-          <div key={post.id}>
-            <h1>{post.title}</h1>
-            <p>{post.body}</p>
-          </div>
-        ))}
-      </div>
+      <section className="container">
+        <div className="posts">
+          {posts.map(post => (
+            <div className="post">
+              <img src={post.cover} alt={post.title} />
+              <div key={post.id} className="post-content">
+                <h1>{post.title}</h1>
+                <p>{post.body}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     );
   }
 }
