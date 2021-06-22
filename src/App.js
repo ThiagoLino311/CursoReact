a72868cc130c5d9d3162386276ca53fd4821abcc
Thiago Lino <thiagolinocsm@hotmail.com>
2021-06-21 23:42:53 -0300

    Primeiro contato utilizando Classe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react'; //Auto import do react
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = { //estado
+    name: 'Thiago Lino',
+    counter: 0
+  };
+
+
+  handlePClick = () => { // função que é chamada quando evento ocorre
+    this.setState({ name: 'THIAGO LINO' }); // acesso a mudança feito através do setState
+  }
+
+  handleAClick = (event) => { // utilizar arrow function quando precisar utilizar o this para não precisar usar o .bind
+    event.preventDefault(); //Tira evento padrão de um elemento e passa a ser executado o evento que eu descrever
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+  }
+
+  render() { //função que renderiza elementos
+    const { name, counter } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handlePClick /* Quando clicado executa a função que altera o estado */}>
+            {name} {counter}
+          </p>
+          <a
+            onClick={this.handleAClick /* Quando clicado executa a função que altera o estado */}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
